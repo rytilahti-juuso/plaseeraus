c946595623f5569693d483f0e26d8dd7a542d55b
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2019-04-20 21:15:20 +0300

    changed the_map to score_table

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -11,43 +11,33 @@ import copy
 import matplotlib.pyplot as plt
 
 def initialize(p_zero, N):
-#    First thing is to create the map you're trying to navigate. I will do this randomly
-#    This will ne the from of a adjacency matrix
-#    In other words, an NxN matrix whre each row and column correspond to an intersection on a map
-#    X_ij, then is equal to the amount of the time that it takes to got from position i to position j
-#    could alse be considered as a distance measure
-#    practically, then we need a matric that has numeric values in it..
-#    the should be some path that don't exist. I will assign these a 0
-#   For instance if you cant get directly from i to j, then X_ij = 0
 
-#    The initialization needs some tuning parameters. One is the proportion of  0's in the final result
-
-    the_map = np.zeros((N,N))
+    score_table = np.zeros((N,N))
     for i in range(0, N):
         for j in range(0,i):
             if random.random()> p_zero:
-                the_map[i][j] = random.random() * 100
-                the_map[j][i] = the_map[i][j]
-    return the_map
+                score_table[i][j] = random.random() * 100
+                score_table[j][i] = score_table[i][j]
+    return score_table
                     
-def create_starting_population(size, the_map):
+def create_starting_population(size, score_table):
 #    This just creates a population of different routes of a fixed size. Pretty straightforward
 
     population = []
     
     for i in range (0, size):
-        population.append(create_new_member(the_map))
+        population.append(create_new_member(score_table))
         
     return population
 
-def fitness(route, the_map):
+def fitness(route, score_table):
     score = 0
     for i in range(1, len(route)):
-        if (the_map[route[i-1]][route[i]] == 0) and i != len(the_map)-1:
+        if (score_table[route[i-1]][route[i]] == 0) and i != len(score_table)-1:
             print("WARNING: INVALID ROUTE")
             print(route)
-            print(the_map)
-        score = score + the_map[route[i-1]][route[i]]
+            print(score_table)
+        score = score + score_table[route[i-1]][route[i]]
     return score
 
 def crossover(a, b):
@@ -72,7 +62,7 @@ def crossover(a, b):
     return child       
 #    return (new_a, new_b)
 
-def mutate(route, probability, the_map):
+def mutate(route, probability, score_table):
        
     for i in range(len(route)):
         if(random.random() < probability):
@@ -86,17 +76,17 @@ def mutate(route, probability, the_map):
     return new_route
 
 #   Creates a new member
-def create_new_member(the_map):    
-    route = np.random.choice(len(the_map), len(the_map), replace=False)
+def create_new_member(score_table):    
+    route = np.random.choice(len(score_table), len(score_table), replace=False)
 #    print(route)
     return route
 
-def score_population(population, the_map):
+def score_population(population, score_table):
     
     scores = []
     
     for i in range(0, len(population)):
-        scores += [fitness(population[i], the_map)]
+        scores += [fitness(population[i], score_table)]
         
     return scores
     
@@ -136,9 +126,9 @@ number_of_winners_to_keep = 2
 number_of_groups = 1
 
 # initialize the map and save it
-the_map = initialize(0, size_of_map)
+score_table = initialize(0, size_of_map)
 # create the starting population
-population = create_starting_population(population_size, the_map)
+population = create_starting_population(population_size, score_table)
 
 last_distance = 1000000000
 # for a large number of iterations do:
@@ -147,26 +137,21 @@ for i in range(0,number_of_iterations):
     new_population = []
 #    print(i)
     # evaluate the fitness of the current population
-    scores = score_population(population, the_map)    
+    scores = score_population(population, score_table)    
     best = population[np.argmax(scores)]
     
     number_of_moves = len(best)
-    distance = fitness(best, the_map)
+    distance = fitness(best, score_table)
     
     if distance != last_distance:
         print('Iteration %i: Best so far is %i steps for a distance of %f' % (i, number_of_moves, distance))
 
-    if(fitness(best, the_map) > fitness(best_of_alltime, the_map)):
+    if(fitness(best, score_table) > fitness(best_of_alltime, score_table)):
         best_of_alltime= best
-        best_score_of_alltime = fitness(best_of_alltime, the_map)
+        best_score_of_alltime = fitness(best_of_alltime, score_table)
         print("best of alltime has changed, it is: ", best_score_of_alltime)
     
-    # allow members of the population to breed based on their relative score; 
-        # i.e., if their score is higher they're more likely to breed
-#    for j in range(0, number_of_couples):  
-#        new_1, new_2 = crossover(population[pick_mate(scores)], population[pick_mate(scores)])
-#        new_population = new_population + [new_1, new_2]
-
+    # allow members of the population to breed
     for j in range(0, number_of_couples):  
 #        new_route = crossover(population[0], population[17])
         new_route = crossover(population[pick_mate(scores)], population[pick_mate(scores)])
@@ -174,7 +159,7 @@ for i in range(0,number_of_iterations):
   
     # mutate
     for j in range(0, len(new_population)):
-        new_population[j] = np.copy(mutate(new_population[j], mutation_probability, the_map))
+        new_population[j] = np.copy(mutate(new_population[j], mutation_probability, score_table))
         
     # keep members of previous generation
     new_population += [population[np.argmax(scores)]]
@@ -184,7 +169,7 @@ for i in range(0,number_of_iterations):
         
     # add new random members
     while len(new_population) < population_size:
-        new_population += [create_new_member(the_map)]
+        new_population += [create_new_member(score_table)]
         
     #replace the old population with a real copy
     population = copy.deepcopy(new_population)
